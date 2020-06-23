d1e86a258c3b34aadf17344a7bf8e924877f3b91
Andreas Wenzel <andreas.m.wenzel@gmail.com>
2020-06-22 22:50:31 -0700

    more descriptive messages for leaveTable

--- realm/services/cards/incoming_webhooks/leaveTable/source.js
+++ realm/services/cards/incoming_webhooks/leaveTable/source.js
@@ -2,15 +2,21 @@ exports = async function (payload, response) {
   const body = EJSON.parse(payload.body.text());
 
   const userId = BSON.ObjectId(body.user); //throws error if body.user is not in right form
-
   const user = await context.functions.execute("removeTableFromUser", userId); //returns user before removing table
-  const resp = { user: user._id };
-  const table = user.currentTable
-    ? await context.functions.execute("removeUserFromTable", user)
-    : null;
-  if (table) {
-    resp.table = table._id;
+
+  let resp;
+  if (user.currentTable) {
+    resp.message = `Table ${user.currentTable} removed from user ${user._id}`;
+    try {
+      await context.functions.execute("removeUserFromTable", user);
+      resp.message.append(` AND user removed from table`);
+    } catch {
+      resp.error = err;
+    }
+  } else {
+    resp.message = "user was not at a table";
   }
+
   response.setHeader("Content-Type", "application/json");
   response.setBody(JSON.stringify(resp));
 };
