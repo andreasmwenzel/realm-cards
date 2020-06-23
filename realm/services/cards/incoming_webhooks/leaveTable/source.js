3ad22a753d13107b13c05a5b4a29760b9bb9337e
Andreas Wenzel <andreas.m.wenzel@gmail.com>
2020-06-22 22:35:07 -0700

    fix no table

--- realm/services/cards/incoming_webhooks/leaveTable/source.js
+++ realm/services/cards/incoming_webhooks/leaveTable/source.js
@@ -4,16 +4,13 @@ exports = async function (payload, response) {
   const userId = BSON.ObjectId(body.user); //throws error if body.user is not in right form
 
   const user = await context.functions.execute("removeTableFromUser", userId); //returns user before removing table
-  console.log(`user: ${JSON.stringify(user)}`);
+  const resp = { user: user._id };
   const table = user.currentTable
     ? await context.functions.execute("removeUserFromTable", user)
     : null;
-
+  if (table) {
+    resp.table = table._id;
+  }
   response.setHeader("Content-Type", "application/json");
-  response.setBody(
-    JSON.stringify({
-      user: user._id,
-      table: table._id,
-    })
-  );
+  response.setBody(JSON.stringify(resp));
 };
