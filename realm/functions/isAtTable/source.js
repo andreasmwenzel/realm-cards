c7c14af9dbe881a38858749f69109287ec1e011f
Andreas Wenzel <andreas.m.wenzel@gmail.com>
2020-06-23 15:17:13 -0700

    log types of

--- realm/functions/isAtTable/source.js
+++ realm/functions/isAtTable/source.js
@@ -1,8 +1,8 @@
 exports = async function (user, players) {
-  console.log(`User: ${user}`);
+  console.log(`User: ${user} of type ${typeof user}`);
   console.log(`players ${JSON.stringify(players)}`);
   for (let i in players) {
-    console.log(`${players[i].id}`);
+    console.log(`checking ${players[i].id} of type ${typeof players[i].id}`);
     if (user === players[i].id) {
       console.log("true");
       return true;
