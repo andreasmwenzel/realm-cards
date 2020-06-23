34e87f218bd5afe84d8463e20b2088f178d4a548
Andreas Wenzel <andreas.m.wenzel@gmail.com>
2020-06-23 15:19:25 -0700

    convert objectId to string

--- realm/functions/isAtTable/source.js
+++ realm/functions/isAtTable/source.js
@@ -2,8 +2,9 @@ exports = async function (user, players) {
   console.log(`User: ${user} of type ${typeof user}`);
   console.log(`players ${JSON.stringify(players)}`);
   for (let i in players) {
-    console.log(`checking ${players[i].id} of type ${typeof players[i].id}`);
-    if (user === players[i].id) {
+    const playerStr = players[i].id.toString();
+    console.log(`checking ${playerStr} of type ${typeof playerStr}`);
+    if (user === playerStr) {
       console.log("true");
       return true;
     }
