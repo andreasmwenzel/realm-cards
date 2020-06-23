89215a11aac5d42a86ec4438ac365d2f7f533106
Andreas Wenzel <andreas.m.wenzel@gmail.com>
2020-06-23 15:29:27 -0700

    for loop fox

--- realm/functions/isAtTable/source.js
+++ realm/functions/isAtTable/source.js
@@ -2,7 +2,7 @@ exports = async function (user, players) {
   console.log(`User: ${user} of type ${typeof user}`);
   console.log(`players ${JSON.stringify(players)}`);
   console.log(`Length: ${players.length}`);
-  for (let i in players) {
+  for (let i = 0; i < players.length; i++) {
     console.log(i);
     const playerStr = players[i].id.toString();
     console.log(`checking ${playerStr} of type ${typeof playerStr}`);
