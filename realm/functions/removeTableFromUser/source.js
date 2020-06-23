5fa3260967f52d88b3e67a1d7425fb3d78388563
Andreas Wenzel <andreas.m.wenzel@gmail.com>
2020-06-22 21:38:07 -0700

    fixed bug removeTableFromUser

--- realm/functions/removeTableFromUser/source.js
+++ realm/functions/removeTableFromUser/source.js
@@ -1,10 +1,10 @@
-exports = async function (userId) {
+exports = async function (user) {
   console.log("running removeTableFromUser");
   const db = context.services.get("mongodb-atlas").db("cards");
   const users = db.collection("users");
 
   return await users.findOneAndUpdate(
-    { _id: userId },
+    { _id: user._id },
     { $unset: { currentTable: "" } },
     {
       projection: { _id: 1, currentTable: 1, username: 1 },
