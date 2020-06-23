3d5bb6d7f328c375f793fa74be37da69a8bfe7b1
Andreas Wenzel <andreas.m.wenzel@gmail.com>
2020-06-23 14:56:35 -0700

    added console logs

--- realm/functions/isAtTable/source.js
+++ realm/functions/isAtTable/source.js
@@ -1,4 +1,6 @@
 exports = async function (user, players) {
+  console.log(`User: ${user}`);
+  console.log(`players ${JSON.stringify(players)}`);
   for (let i in players) {
     if (BSON.ObjectId(user) === players[i].id) {
       return true;
