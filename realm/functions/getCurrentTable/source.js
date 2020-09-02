4f8f4dfcee2596e7566b754012e18e15d9d39047
Andreas Wenzel <andreas.m.wenzel@gmail.com>
2020-09-01 17:07:44 -0700

    update getCurrentTable function

--- realm/functions/getCurrentTable/source.js
+++ realm/functions/getCurrentTable/source.js
@@ -1,6 +1,7 @@
 exports = async function () {
   const db = context.services.get("mongodb-atlas").db("cards");
   const users = db.collection("users");
-  return await users.findOne({ _id: context.user.id })
+  console.log(context.user.id)
+  return await users.findOne({ _id: ObjectID(context.user.id) })
   //return context.user;
 };
